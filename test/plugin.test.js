12982c9bec87fb5362c07fbc1614cad323024d14
kisenka <qtuzov@gmail.com>
2017-04-13 17:01:41 +0300

    Update plugin tests

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,8 +1,13 @@
+const path = require('path');
+const fs = require('fs');
 const { strictEqual, equal, ok } = require('assert');
 const merge = require('lodash.merge');
-const { compile, fixturesPath, loaderPath } = require('./tests-utils');
+
+const { compile, createCompiler, fixturesPath, loaderPath, rootDir } = require('./tests-utils');
 const Plugin = require('../lib/plugin');
 const ExtractPlugin = require('extract-text-webpack-plugin');
+const InMemoryCompiler = require('webpack-toolkit').InMemoryCompiler;
+const MemoryFS = require('memory-fs');
 
 const spriteLoaderRule = (cfg) => {
   return merge({
@@ -23,14 +28,54 @@ const extractPluginRule = () => {
  */
 
 describe('plugin', () => {
+  const svg = '<svg></svg>';
+  let CSSExtractor;
+
+  beforeEach(() => CSSExtractor = new ExtractPlugin('[name].css'));
+
   it('should properly detect modules to extract', async () => {
-    const compilation = await compile({
+    const { assets } = await compile({
       entry: './entry',
       module: { rules: [spriteLoaderRule()] },
-      plugins: [new Plugin()]
+      plugins: [new Plugin()],
+      files: {
+        'entry.js': 'require("./image.svg")',
+        'image.svg': svg
+      }
     });
 
-    const assetsCount = Object.keys(compilation.assets).length;
-    equal(assetsCount, 1);
+    ok(Object.keys(assets).length === 1);
+  });
+
+  it('should allow to specify custom sprite filename', async () => {
+    const spriteFilename = 'qwe.svg';
+
+    const { assets } = await compile({
+      entry: './styles.css',
+      module: { rules: [
+        spriteLoaderRule({ options: { spriteFilename } }),
+        extractPluginRule()
+      ] },
+      plugins: [new Plugin(), CSSExtractor]
+    });
+
+    ok(Object.keys(assets).length === 3);
+    ok(spriteFilename in assets);
+  });
+
+  it('should support [chunkname] placeholder in sprite filename', async () => {
+
+  });
+
+  it('should replace with proper publicPath', () => {
+
+  });
+
+  it('should work with html-loader', () => {
+
+  });
+
+  it('should warn if several rules applies to module', () => {
+
   });
 });
--- test/tests-utils.js
+++ test/tests-utils.js
@@ -1,25 +1,59 @@
 const path = require('path');
 const { ok } = require('assert');
+const Promise = require('bluebird');
 const merge = require('lodash.merge');
-const {
-  InMemoryCompiler,
-  createCachedInputFileSystem
-} = require('webpack-toolkit');
+const { InMemoryCompiler, MemoryFileSystem, createCachedInputFileSystem } = require('webpack-toolkit');
 
 const fixturesPath = path.resolve(__dirname, 'fixtures');
 const loaderPath = require.resolve('../lib/loader.js');
+const rootDir = path.resolve(__dirname, '..');
 
+/**
+ * @param {Object} [config]
+ * @return {InMemoryCompiler}
+ */
+function createCompiler(config = {}) {
+  const cfg = merge({
+    context: fixturesPath
+  }, config);
+
+  if (!cfg.files) {
+    const inputFS = createCachedInputFileSystem();
+    return Promise.resolve(
+      new InMemoryCompiler(cfg, { inputFS })
+    );
+  }
+
+  const promisedInputFS = new MemoryFileSystem();
+
+  return Promise.map(Object.keys(cfg.files), (filename) => {
+    const rawContent = cfg.files[filename];
+    const content = rawContent instanceof Buffer === false ? new Buffer(rawContent) : rawContent;
+    const filepath = path.resolve(cfg.context, filename);
+
+    return promisedInputFS.writep(filepath, content);
+  })
+    .then(() => {
+      delete cfg.files;
+      return new InMemoryCompiler(cfg, { inputFS: promisedInputFS.fs });
+    });
+}
+
+/**
+ * @param {Object} [config]
+ * @return {Promise<Compilation>}
+ */
 function compile(config) {
-  const cfg = merge({ context: fixturesPath }, config);
-  const inputFS = createCachedInputFileSystem();
-  return new InMemoryCompiler(cfg, { inputFS }).run(false);
+  return createCompiler(config).then(compiler => compiler.run());
 }
 
 const notOk = (value, message) => ok(!value, message);
 
 module.exports = {
+  createCompiler,
   compile,
   notOk,
   fixturesPath,
-  loaderPath
+  loaderPath,
+  rootDir
 };
