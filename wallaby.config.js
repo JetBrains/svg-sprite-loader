7d83645c7bc4fadc59d6e79d27bca9393611a21b
kisenka <qtuzov@gmail.com>
2017-04-15 18:28:04 +0300

    Add runtime deps & compile configs

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,13 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies,global-require */
 module.exports = () => {
   const config = {
     files: [
-      { pattern: 'lib/*.js' },
       { pattern: 'lib/**/*.js' },
-      { pattern: 'test/tests-*.js', instrument: false },
+      { pattern: 'test/_*.js', instrument: false },
 
       // static files
-      { pattern: 'test/fixtures/*', load: false, instrument: false },
-      { pattern: 'test/fixtures/**/*', load: false, instrument: false }
+      { pattern: 'test/fixtures/**', load: false, instrument: false },
+      { pattern: 'package.json', load: false }
     ],
 
     tests: [
@@ -26,13 +26,23 @@ module.exports = () => {
       const mocha = wallaby.testFramework;
       mocha.ui('bdd');
 
-      // eslint-disable-next-line import/no-extraneous-dependencies,global-require
       const webpackToolkit = require('webpack-toolkit');
+      const { PACKAGE_NAME } = require('./lib/config');
+
       const OldInMemoryCompiler = webpackToolkit.InMemoryCompiler;
       if (!OldInMemoryCompiler.patched) {
         // eslint-disable-next-line no-multi-assign
         const NewInMemoryCompiler = webpackToolkit.InMemoryCompiler = function patched(cfg) {
-          cfg.resolveLoader = { modules: [`${wallaby.localProjectDir}/node_modules`] };
+          cfg.resolve = {
+            alias: {
+              [PACKAGE_NAME]: `${wallaby.localProjectDir}`
+            }
+          };
+
+          cfg.resolveLoader = {
+            modules: [`${wallaby.localProjectDir}/node_modules`]
+          };
+
           NewInMemoryCompiler.prototype = OldInMemoryCompiler.prototype;
           // eslint-disable-next-line prefer-rest-params
           return OldInMemoryCompiler.apply(this, arguments);
