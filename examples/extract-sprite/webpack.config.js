8c0c60af28d299e63c0544057414f7626f940401
kisenka <qtuzov@gmail.com>
2017-04-23 16:56:32 +0300

    Update yarn.lock

---
+++ examples/extract-sprite/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const merge = require('../node_modules/deepmerge');
+const TextExtractPlugin = require('../node_modules/extract-text-webpack-plugin');
+const baseConfig = require('../base-webpack.config');
+const SpritePlugin = require('../../plugin');
+
+const CSSExtractor = new TextExtractPlugin('[name].css');
+const HTMLExtractor = new TextExtractPlugin('[name].html');
+
+const config = merge(baseConfig, {
+  context: __dirname,
+
+  entry: './main.js',
+
+  output: {
+    path: path.resolve(__dirname, 'build')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: CSSExtractor.extract({ use: 'css-loader' })
+      },
+      {
+        test: /\.html$/,
+        loader: HTMLExtractor.extract({ use: 'html-loader' })
+      }
+    ]
+  },
+
+  plugins: [
+    CSSExtractor,
+    HTMLExtractor,
+    new SpritePlugin()
+  ]
+});
+
+module.exports = config;
