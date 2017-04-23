c135faa5e136026bdb6fe416ab6ba9ed32ed8a2f
kisenka <qtuzov@gmail.com>
2017-04-23 23:06:28 +0300

    Add browser sprite runtime example

--- examples/browser-sprite/build/main.js
+++ examples/browser-sprite/build/main.js
@@ -665,11 +665,42 @@ var BrowserSprite = (function (Sprite$$1) {
   return BrowserSprite;
 }(Sprite));
 
+var ready$1 = createCommonjsModule(function (module) {
+/*!
+  * domready (c) Dustin Diaz 2014 - License MIT
+  */
+!function (name, definition) {
+
+  { module.exports = definition(); }
+
+}('domready', function () {
+
+  var fns = [], listener
+    , doc = document
+    , hack = doc.documentElement.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState);
+
+
+  if (!loaded)
+  { doc.addEventListener(domContentLoaded, listener = function () {
+    doc.removeEventListener(domContentLoaded, listener);
+    loaded = 1;
+    while (listener = fns.shift()) { listener(); }
+  }); }
+
+  return function (fn) {
+    loaded ? setTimeout(fn, 0) : fns.push(fn);
+  }
+
+});
+});
+
 var sprite = new BrowserSprite();
 
-document.addEventListener('DOMContentLoaded', function () {
+ready$1(function () {
   sprite.mount(document.body, true);
-}, false);
+});
 
 return sprite;
 
