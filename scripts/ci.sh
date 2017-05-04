45899ef0ba1af93b17eb317782936138bbaafd34
kisenka <qtuzov@gmail.com>
2017-05-04 17:46:49 +0300

    ci(travis): update build script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 yarn bootstrap
-./select-env.sh webpack-1
 yarn lint
 yarn build:runtime
-yarn coverage
-./select-env.sh webpack-2
+
+yarn env:webpack-1
+yarn test
+yarn env:webpack-2
 yarn coverage
