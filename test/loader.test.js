a38fdccefa8041a45852c5c6831f89e728ba490b
kisenka <qtuzov@gmail.com>
2017-05-07 17:28:49 +0300

    feat(interop): extract-text-webpack-plugin & html-webpack-plugin interop

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const { isWebpack1 } = require('../lib/utils');
+const { loaderPath, fixturesPath } = require('./_config');
 const {
   rule,
   rules,
   multiRule,
   svgRule,
   compile,
-  loaderPath,
-  fixturesPath,
   extractPlugin,
   extractCSSRule,
   extractHTMLRule,
@@ -64,6 +67,10 @@ describe('loader and plugin', () => {
   });
 
   describe('extract mode', () => {
+    function cssRule() {
+      return rule({ test: /\.css$/, loader: 'css-loader' });
+    }
+
     describe('exceptions', () => {
       it('should emit error if loader used without plugin in extract mode', async () => {
         const { errors } = await compileAndNotReject({
@@ -98,69 +105,149 @@ describe('loader and plugin', () => {
       });
     });
 
-    describe('interoperability', () => {
-      describe('extract-text-webpack-plugin', () => {
-        it('should properly extract sprite from extractable CSS', async () => {
-          const extractor = extractPlugin('[name].css');
-          const { assets } = await compile({
-            entry: './entry.css',
-            module: rules(
-              svgRule(),
-              extractCSSRule(extractor)
-            ),
-            plugins: [new SpritePlugin(), extractor]
-          });
-
-          Object.keys(assets).should.be.lengthOf(3);
-          assets.should.have.property(defaultSpriteFilename);
-          assets['main.css'].source().should.includes(defaultSpriteFilename);
+    describe('extract-text-webpack-plugin interop', () => {
+      it('should properly extract sprite from extractable CSS', async () => {
+        const extractor = extractPlugin('[name].css');
+        const { assets } = await compile({
+          entry: './entry.css',
+          module: rules(
+            svgRule(),
+            extractCSSRule(extractor)
+          ),
+          plugins: [new SpritePlugin(), extractor]
         });
 
-        it('should work properly with `allChunks: true` config option', async () => {
-          const spriteFilename = 'qwe.svg';
-          const extractor = extractPlugin('[name].css', { allChunks: true });
+        Object.keys(assets).should.be.lengthOf(3);
+        assets.should.have.property(defaultSpriteFilename);
+        assets['main.css'].source().should.contain(defaultSpriteFilename);
+      });
 
-          const { assets } = await compile({
-            entry: {
-              styles: './styles.css',
-              styles2: './styles2.css'
-            },
-            module: rules(
-              svgRule({ spriteFilename }),
-              extractCSSRule(extractor)
-            ),
-            plugins: [
-              new SpritePlugin(),
-              extractor
-            ]
-          });
+      it('should work properly with `allChunks: true` config option', async () => {
+        const spriteFilename = 'qwe.svg';
+        const extractor = extractPlugin('[name].css', { allChunks: true });
 
-          Object.keys(assets).should.be.lengthOf(5);
-          assets.should.have.property(spriteFilename);
-          assets['styles.css'].source().should.includes(spriteFilename);
-          assets['styles2.css'].source().should.includes(spriteFilename);
+        const { assets } = await compile({
+          entry: {
+            styles: './styles.css',
+            styles2: './styles2.css'
+          },
+          module: rules(
+            svgRule({ spriteFilename }),
+            extractCSSRule(extractor)
+          ),
+          plugins: [
+            new SpritePlugin(),
+            extractor
+          ]
         });
 
-        it('should emit sprite for each extracted chunk if [chunkname] provided in `spriteFilename`', async () => {
-          const extractor = extractPlugin('[name].css');
-          const { assets } = await compile({
-            entry: {
-              'entry-with-styles': './entry-with-styles',
-              'entry-with-styles2': './entry-with-styles2'
-            },
-            module: rules(
-              svgRule({ extract: true, spriteFilename: '[chunkname].svg' }),
-              extractCSSRule(extractor)
-            ),
-            plugins: [new SpritePlugin(), extractor]
-          });
+        Object.keys(assets).should.be.lengthOf(5);
+        assets.should.have.property(spriteFilename);
+        assets['styles.css'].source().should.contain(spriteFilename);
+        assets['styles2.css'].source().should.contain(spriteFilename);
+      });
 
-          Object.keys(assets).should.be.lengthOf(6);
-          assets.should.have.property('entry-with-styles.svg');
-          assets.should.have.property('entry-with-styles2.svg');
-          assets['entry-with-styles.css'].source().should.includes('entry-with-styles.svg');
-          assets['entry-with-styles2.css'].source().should.includes('entry-with-styles2.svg');
+      /**
+       * TODO test in webpack 1 with allChunks: true and [chunkname]
+       * Currently [chunkname] will not work in webpack 1 with extract-text-webpack-plugin(allChunks: true)
+       */
+      it('should emit sprite for each extracted chunk if [chunkname] provided in `spriteFilename`', async () => {
+        const extractor = extractPlugin('[name].css', { allChunks: true });
+        const { assets } = await compile({
+          entry: {
+            entry: './entry-with-styles',
+            entry2: './entry-with-styles2'
+          },
+          module: rules(
+            svgRule({ spriteFilename: '[chunkname].svg' }),
+            extractCSSRule(extractor)
+          ),
+          plugins: [extractor, new SpritePlugin()]
         });
+
+        /**
+         * Chunk.name is undefined in webpack 1 with extract-text-webpack-plugin(allChunks: true),
+         * so it will be only 1 sprite file instead of 2
+         */
+        Object.keys(assets).should.be.lengthOf(isWebpack1 ? 5 : 6);
+        if (isWebpack1) {
+          return;
+        }
+
+        assets.should.have.property('entry.svg');
+        assets.should.have.property('entry2.svg');
+        assets['entry.css'].source().should.contain('entry.svg');
+        assets['entry2.css'].source().should.contain('entry2.svg');
+      });
+
+      it('should work in combination with CommonsChunkPlugin', async () => {
+        const extractor = extractPlugin('[name].css');
+        const { assets } = await compile({
+          context: path.resolve(fixturesPath, 'extract-text-webpack-plugin/with-commons-chunk-plugin'),
+          entry: {
+            entry: './entry',
+            entry2: './entry2',
+            entry3: './entry3'
+          },
+          module: rules(
+            svgRule({ spriteFilename: '[chunkname].svg' }),
+            extractCSSRule(extractor)
+          ),
+          plugins: [
+            extractor,
+            new SpritePlugin(),
+            new CommonsChunkPlugin({
+              name: 'common'
+            })
+          ]
+        });
+
+        assets['common.css'].source().should.contain('common.svg');
+      });
+    });
+
+    describe('html-loader interop', () => {
+      it('should work in combination with html-loader and extract-text-webpack-plugin', async () => {
+        const spriteFilename = defaultSpriteFilename;
+        const extractor = extractPlugin('[name].html');
+
+        const { assets } = await compile({
+          entry: './entry.html',
+          module: rules(
+            svgRule({ spriteFilename }),
+            extractHTMLRule(extractor)
+          ),
+          plugins: [
+            new SpritePlugin(),
+            extractor
+          ]
+        });
+
+        assets['main.html'].source().should.contain(`img src="${spriteFilename}#`);
+      });
+    });
+
+    describe('html-webpack-plugin interop', () => {
+      it('should work', async () => {
+        const { assets } = await compile({
+          entry: './entry',
+          module: rules(
+            svgRule({ extract: true }),
+            rule({
+              test: /\.html$/,
+              loader: 'html-loader'
+            })
+          ),
+          plugins: [
+            new SpritePlugin(),
+            new HtmlPlugin({
+              filename: 'index.html',
+              template: path.resolve(fixturesPath, 'html-webpack-plugin/template.html')
+            })
+          ]
+        });
+
+        assets['index.html'].source().should.contain(defaultSpriteFilename);
       });
     });
 
@@ -183,23 +270,20 @@ describe('loader and plugin', () => {
         entry: './styles.css',
         module: rules(
           svgRule({ spriteFilename }),
-          extractCSSRule()
+          cssRule()
         ),
-        plugins: [
-          CSSExtractor,
-          new SpritePlugin()
-        ]
+        plugins: [new SpritePlugin()]
       });
 
-      Object.keys(assets).should.be.lengthOf(3);
+      Object.keys(assets).should.be.lengthOf(2);
       assets.should.have.property(spriteFilename);
     });
 
     it('should emit sprite for each chunk if [chunkname] presented in sprite filename', async () => {
       const { assets } = await compile({
         entry: {
-          styles: './styles.css',
-          styles2: './styles2.css'
+          entry: './entry-with-styles',
+          entry2: './entry-with-styles2'
         },
         module: rules(
           multiRule({
@@ -209,19 +293,29 @@ describe('loader and plugin', () => {
               'svgo-loader'
             ]
           }),
-          extractCSSRule()
+          cssRule()
         ),
-        plugins: [
-          CSSExtractor,
-          new SpritePlugin()
-        ]
+        plugins: [new SpritePlugin()]
       });
 
-      Object.keys(assets).should.be.lengthOf(6);
-      assets.should.have.property('styles-sprite.svg');
-      assets.should.have.property('styles2-sprite.svg');
+      Object.keys(assets).should.be.lengthOf(4);
+      assets.should.have.property('entry-sprite.svg');
+      assets.should.have.property('entry2-sprite.svg');
     });
 
+    it('should allow to use [spritehash] substitution token in `spriteFilename`', async () => {
+      const { assets } = await compile({
+        entry: './entry',
+        module: rules(
+          svgRule({ extract: true, spriteFilename: 'sprite-[spritehash:8].svg' })
+        ),
+        plugins: [new SpritePlugin()]
+      });
+
+      assets.should.have.property('sprite-f016181b.svg');
+    });
+
+    // Fails when webpack buildin runtime will change
     it('should replace with proper publicPath', async () => {
       const publicPath = '/olala/';
       const spriteFilename = defaultSpriteFilename;
@@ -235,25 +329,7 @@ describe('loader and plugin', () => {
         plugins: [new SpritePlugin()]
       });
 
-      assets['main.js'].source().should.contain(publicPath + spriteFilename);
-    });
-
-    it('should work with html-loader', async () => {
-      const spriteFilename = defaultSpriteFilename;
-
-      const { assets } = await compile({
-        entry: './page.html',
-        module: rules(
-          svgRule({ spriteFilename }),
-          extractHTMLRule()
-        ),
-        plugins: [
-          new SpritePlugin(),
-          HTMLExtractor
-        ]
-      });
-
-      assets['main.html'].source().should.contain(`img src="${spriteFilename}#`);
+      assets['main.js'].source().should.contain(`__webpack_require__.p + "${spriteFilename}`);
     });
 
     it('should emit only built chunks', () => {
