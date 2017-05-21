a03820c08f7999c91732a3d70692154ef5461f71
kisenka <qtuzov@gmail.com>
2017-05-21 20:08:04 +0300

    chore(tools): migrate to cz-customizable
    Replace default commitizen adapter cz-conventional-changelog with cz-customizable with project
    specific settings

---
+++ .cz-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  types: [
+    {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'docs',     name: 'docs:     Documentation only changes'},
+    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'perf',     name: 'perf:     A code change that improves performance'},
+    {value: 'test',     name: 'test:     Adding missing tests'},
+    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation'},
+    {value: 'revert',   name: 'revert:   Revert to a commit'},
+    {value: 'WIP',      name: 'WIP:      Work in progress'}
+  ],
+
+  scopes: [
+    {name: 'loader'},
+    {name: 'plugin'},
+    {name: 'utils'},
+    {name: 'configuration'},
+    {name: 'examples'},
+    {name: 'scripts'},
+    {name: 'build'},
+    {name: 'tools'},
+    {name: 'ci'},
+  ],
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix']
+};
