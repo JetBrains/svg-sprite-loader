13e3d471937eb1f02037aac2a5e2c1b5202ff3d9
kisenka <qtuzov@gmail.com>
2017-08-15 16:41:56 +0300

    fix(runtime): store global sprite object in window which allows to mount symbols in one place betwee
    ISSUES CLOSED: #8

--- runtime/browser-sprite.js
+++ runtime/browser-sprite.js
@@ -1,23 +1,21 @@
 import BrowserSprite from 'svg-baker-runtime/src/browser-sprite';
 import domready from 'domready';
 
-const sprite = new BrowserSprite();
+const globaVarName = '__SVG_SPRITE__';
+const isSpriteExists = !!window[globaVarName];
+
+// eslint-disable-next-line import/no-mutable-exports
+let sprite;
+
+if (isSpriteExists) {
+  sprite = window[globaVarName];
+} else {
+  sprite = new BrowserSprite();
+  window[globaVarName] = sprite;
+}
 
 const loadSprite = () => {
-  const svg = sprite.mount(document.body, true);
-
-  // :WORKAROUND:
-  // IE doesn't evaluate <style> tags in SVGs that are dynamically added to the page.
-  // This trick will trigger IE to read and use any existing SVG <style> tags.
-  //
-  // Reference: https://github.com/iconic/SVGInjector/issues/23
-  const ua = window.navigator.userAgent || '';
-  if (ua.indexOf('Trident') > 0 || ua.indexOf('Edge/') > 0) {
-    const styles = svg.querySelectorAll('style');
-    for (let i = 0, l = styles.length; i < l; i += 1) {
-      styles[i].textContent += '';
-    }
-  }
+  sprite.mount(document.body, true);
 };
 
 if (document.body) {
