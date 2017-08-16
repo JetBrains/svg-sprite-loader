4056d7bc362b8129dc7cad78b30bf257fb3bcccb
kisenka <qtuzov@gmail.com>
2017-08-16 14:43:39 +0300

    feat(runtime): add ability to create sprite from existing DOM node
    ISSUES CLOSED: #165

--- runtime/browser-sprite.js
+++ runtime/browser-sprite.js
@@ -1,21 +1,33 @@
 import BrowserSprite from 'svg-baker-runtime/src/browser-sprite';
 import domready from 'domready';
 
-const globaVarName = '__SVG_SPRITE__';
-const isSpriteExists = !!window[globaVarName];
+const spriteNodeId = '__SVG_SPRITE_NODE__';
+const spriteGlobalVarName = '__SVG_SPRITE__';
+const isSpriteExists = !!window[spriteGlobalVarName];
 
 // eslint-disable-next-line import/no-mutable-exports
 let sprite;
 
 if (isSpriteExists) {
-  sprite = window[globaVarName];
+  sprite = window[spriteGlobalVarName];
 } else {
-  sprite = new BrowserSprite();
-  window[globaVarName] = sprite;
+  sprite = new BrowserSprite({ attrs: { id: spriteNodeId } });
+  window[spriteGlobalVarName] = sprite;
 }
 
 const loadSprite = () => {
-  sprite.mount(document.body, true);
+  /**
+   * Check for page already contains sprite node
+   * If found - attach to and reuse it's content
+   * If not - render and mount the new sprite
+   */
+  const existing = document.getElementById(spriteNodeId);
+
+  if (existing) {
+    sprite.attach(existing);
+  } else {
+    sprite.mount(document.body, true);
+  }
 };
 
 if (document.body) {
